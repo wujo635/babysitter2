0d0e29748165558e7e7a70e0e94299e2d80d6fdd
wujo635 <wujo635@gmail.com>
2019-01-18 00:44:54 -0500

    Initial submission for testing

---
+++ src/main/java/kata/BabysitterCalculator.java
@@ -0,0 +1,17 @@
+package kata;
+
+public class BabysitterCalculator {
+    public int calculate(int start, int end) {
+        if (isNotWithinWorkingHours(start) || isNotWithinWorkingHours(end)) {
+            return -1;
+        }
+        if (end < start) {
+            return -1;
+        }
+        return (end - start);
+    }
+
+    private boolean isNotWithinWorkingHours(int time) {
+        return time < 17 && time > 4;
+    }
+}
---
+++ src/test/java/kata/BabysitterCalculatorTest.java
@@ -0,0 +1,61 @@
+package kata;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BabysitterCalculatorTest {
+
+    private BabysitterCalculator calculator = new BabysitterCalculator();
+
+    @Test
+    public void calculatePayForOneHourOfWork() {
+        assertEquals(1, calculator.calculate(17, 18));
+    }
+
+    @Test
+    public void calculatePayForTwoHoursWork() {
+        assertEquals(2, calculator.calculate(17, 19));
+    }
+
+    @Test
+    public void startTimeEqualEndTimeReturnsZero() {
+        assertEquals(0, calculator.calculate(17, 17));
+    }
+
+    @Test
+    public void startTimeCannotBeEarlierThan5pmReturnsNegative1() {
+        assertEquals(-1, calculator.calculate(16, 18));
+    }
+
+    @Test
+    public void startTimeCanBeLateAs4am() {
+        assertEquals(0, calculator.calculate(4, 4));
+    }
+
+    @Test
+    public void endTimeOutOfBoundsAt5amReturnsNegative1() {
+        assertEquals(-1, calculator.calculate(4, 5));
+    }
+
+    @Test
+    public void endTimeOutOfBoundsAt3pmReturnsNegative1() {
+        assertEquals(-1, calculator.calculate(4, 15));
+    }
+
+    @Test
+    public void endOneHourBeforeStartReturnsNegative1() {
+        assertEquals(-1, calculator.calculate(18, 17));
+    }
+
+    @Test
+    public void endTwoHoursBeforeStartReturnsNegative1() {
+        assertEquals(-1, calculator.calculate(19, 17));
+    }
+
+    @Test
+    public void endAtMidnightAndStartAt11PMReturns1() {
+        assertEquals(1, calculator.calculate(23, 0));
+    }
+}
+
